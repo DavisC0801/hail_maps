6a38c8203242a5a185c23d0435abad56512c87ef
davisc0801 <davisc1014@gmail.com>
2019-10-15 18:51:22 -0600

    Add client and address models

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length = 200)
+    phone_number = models.CharField(max_length = 13)
+
+class Address(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    street = models.CharField(max_length = 200)
+    city = models.CharField(max_length = 200)
+    state = models.CharField(max_length = 30)
+    zip = models.CharField(max_length = 10)
