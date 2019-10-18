cf3b0a1bb8a9d26e062ebdb1bdeed09e8e87c5a5
davisc0801 <davisc1014@gmail.com>
2019-10-17 21:25:45 -0600

    Add map index view

--- maps/views.py
+++ maps/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Client
 
-# Create your views here.
+def index(request):
+    client_list = Client.objects.order_by('name')[:5]
+    context = { 'client_list': client_list }
+    return render(request, 'index.html', context)
