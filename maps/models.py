20dbd977f5a9b14f6c3d68f650e2245922e3fa37
davisc0801 <davisc1014@gmail.com>
2019-10-17 20:08:50 -0600

    Add lat/long fields and method to return an object string

--- maps/models.py
+++ maps/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length = 200)
     phone_number = models.CharField(max_length = 13)
+    def __str__(self):
+        return self.name
 
 class Address(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
@@ -10,3 +12,7 @@ class Address(models.Model):
     city = models.CharField(max_length = 200)
     state = models.CharField(max_length = 30)
     zip = models.CharField(max_length = 10)
+    longitude = models.CharField(max_length = 200, null=True)
+    latitude = models.CharField(max_length = 200, null=True)
+    def __str__(self):
+        return str(self.street)+", "+str(self.city)+", "+str(self.state)
