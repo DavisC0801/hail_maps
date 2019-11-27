88d30a160e0f9bc7a84402f76459dd0e657f6120
davisc0801 <davisc1014@gmail.com>
2019-11-27 16:45:39 -0700

    Refactor view to pass address data instead of client data

--- maps/views.py
+++ maps/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Client
+from .models import Address
 
 def index(request):
-    client_list = Client.objects.order_by('name')[:5]
-    context = { 'client_list': client_list }
+    address_list = Address.objects.order_by('zip')[:5]
+    context = { 'address_list': address_list }
     return render(request, 'index.html', context)
